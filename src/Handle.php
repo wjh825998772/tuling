7a013550783f8caf61c47fb84200f7441529a8a8
jh-wen <jh-wen@haihangyun.com>
2018-01-16 13:53:42 +0800

    添加注释

--- src/Handle.php
+++ src/Handle.php
@@ -11,14 +11,22 @@ use Curl\Curl;
  */
 class Handle
 {
-    protected $config;
-
+    /**
+     * Handle constructor.
+     * @param $config
+     */
     public function __construct($config)
     {
         $this->appUrl = $config['app_url'];
         $this->appKey = $config['app_key'];
     }
 
+    /**
+     * 图灵机器人核心处理
+     *
+     * @param $message
+     * @return mixed
+     */
     public function handle($message)
     {
         $curl = new Curl();
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,6 +6,11 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 class ServiceProvider extends BaseServiceProvider
 {
 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->app->singleton('tuling', function($app) {
@@ -14,11 +19,21 @@ class ServiceProvider extends BaseServiceProvider
         });
     }
 
+    /**
+     * 发布配置文件
+     *
+     * @return void
+     */
     public function boot()
     {
         $this->publishes([$this->configPath() => config_path('tuling.php') ]);
     }
 
+    /**
+     * 配置文件路径
+     *
+     * @return string
+     */
     protected function configPath()
     {
         return __DIR__ . '/../config/tuling.php';
